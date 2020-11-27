265ef673997542037f66400900605648459a02ed
Glazynov A <glazunov_ay@lemz-t.ru>
2020-11-27 16:28:19 +0700

    fix color init

--- lib/color_scheme.dart
+++ lib/color_scheme.dart
@@ -7,27 +7,26 @@ class AppColorScheme {
     begin: Alignment.topCenter,
     end:   Alignment.bottomCenter,
     
-    colors: [Color(0xB6C6FF), Color(0xBDA3F5)]
+    colors: [Color(0xFFB6C6FF), Color(0xFFBDA3F5)]
   );
   
   static final LinearGradient backgroudGradientAlternative = new LinearGradient(
     begin: Alignment.topCenter,
     end:   Alignment.bottomCenter,
     
-    colors: [Color(0x1B1B1B), Color(0x515151)]
+    colors: [Color(0xFF1B1B1B), Color(0xFF515151)]
   ); 
 
   static final LinearGradient weatherWidgetBackground = new LinearGradient(
     begin: Alignment.bottomLeft,
     end: Alignment.topRight,
-
-    colors: [Color(0xA282FF), Color(0x8D65FF)]
+    colors: [Color(0xFFA282FF), Color(0xFF8D65FF)]
   );
 
   // Colors.
-  static final Color mainColor = Color(0xFBFBFE);
-  static final Color alternativeMainColor = Color(0xA282FF);
+  static final Color mainColor = Color(0xFFFBFBFE);
+  static final Color alternativeMainColor = Color(0xFFA282FF);
 
-  static final Color mainFontColor = Color(0x9069FF);
-  static final Color alternativeMainFontColor = Color(0xFBFBFE);
+  static final Color mainFontColor = Color(0xFFFBFBFE);
+  static final Color alternativeMainFontColor = Color(0xFFFBFBFE);
 }
